0f3b3212a357fad4d8e53d53a39d0aab2c639e48
Thomas <nyumat18@gmail.com>
2023-02-12 23:52:29 -0800

    Updated Refs To Include Nyumat

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,19 +8,19 @@ class MyDocument extends Document {
           <link rel="icon" href="/favicon.ico" />
           <meta
             name="description"
-            content="See pictures from Next.js Conf and the After Party."
+            content="See pictures from Tom Nyuma's (Nyumat) Band Career."
           />
           <meta property="og:site_name" content="nextjsconf-pics.vercel.app" />
           <meta
             property="og:description"
-            content="See pictures from Next.js Conf and the After Party."
+            content="See pictures from Tom Nyuma's (Nyumat) Band Career."
           />
-          <meta property="og:title" content="Next.js Conf 2022 Pictures" />
+          <meta property="og:title" content="Tom Nyuma's Marching Band Archive" />
           <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:title" content="Next.js Conf 2022 Pictures" />
+          <meta name="twitter:title" content="Tom Nyuma's Marching Band Archive" />
           <meta
             name="twitter:description"
-            content="See pictures from Next.js Conf and the After Party."
+            content="See pictures from Tom Nyuma's Band Career."
           />
         </Head>
         <body className="bg-black antialiased">
--- pages/p/[photoId].tsx
+++ pages/p/[photoId].tsx
@@ -17,7 +17,7 @@ const Home: NextPage = ({ currentPhoto }: { currentPhoto: ImageProps }) => {
   return (
     <>
       <Head>
-        <title>Next.js Conf 2022 Photos</title>
+        <title>Tom Nyuma's Marching Band Archive</title>
         <meta property="og:image" content={currentPhotoUrl} />
         <meta name="twitter:image" content={currentPhotoUrl} />
       </Head>
