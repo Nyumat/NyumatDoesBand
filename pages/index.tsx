5ac6bf3a15f5bdcab2fd2acff9d7a71c41198bb4
Thomas <nyumat18@gmail.com>
2023-02-14 00:01:54 -0800

    Audited Old SVG, Removed Name From Intro

--- pages/index.tsx
+++ pages/index.tsx
@@ -51,16 +51,19 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
               <span className="flex max-h-full max-w-full items-center justify-center">
                 <Bridge />
               </span>
-              <span className="absolute left-0 right-0 bottom-0 h-[400px] bg-gradient-to-b from-black/0 via-black to-black"></span>
+              {/* <span className="absolute left-0 right-0 bottom-0 h-[400px] bg-gradient-to-b from-black/0 via-black to-black"></span> */}
             </div>
             {/* <Logo /> */}
             <h1 className="mt-8 mb-4 text-base font-bold uppercase tracking-widest">
               Nyumat Does Band
             </h1>
             <p className="max-w-[40ch] text-white/75 sm:max-w-[32ch]">
-              Welcome to my (Tom Nyuma's) marching band archive. Here you will
-              find photos from my time in marching band from college to DCI to
-              high school. I hope you enjoy!
+              Welcome to my marching band archive. Here you will find photos
+              from my time in marching band from college to DCI to high school.
+              I hope you enjoy!
+            </p>
+            <p className="max-w-[40ch] text-white/75 sm:max-w-[32ch]">
+              206, Go Beavs, Go Lions, Splooie.
             </p>
             {/* <a
               className="pointer z-10 mt-6 rounded-lg border border-white bg-white px-3 py-2 text-sm font-semibold text-black transition hover:bg-white/10 hover:text-white md:mt-4"
